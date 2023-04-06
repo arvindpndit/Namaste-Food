f1c5398dd67f7b33cd730afc1352bf640b9dde76
arvindpndit <arvindpandit9808@gmail.com>
2023-04-06 11:27:51 +0530

    restaurant menu is added

--- src/components/RestaurantItemCard.js
+++ src/components/RestaurantItemCard.js
@@ -1,7 +1,22 @@
 const RestaurantItemCard = (props) => {
-  const info = props.info;
-  console.log(info);
-  return <div>hello</div>;
+  return (
+    <div>
+      <div className="flex w-9/12 mx-auto gap-4 pt-5">
+        <div className="w-9/12">
+          <h2 className="font-xl font-bold">{props.item.card.info.name}</h2>
+          <p>{props?.item?.card?.info?.description}</p>
+        </div>
+
+        <img
+          className="w-52 h-32"
+          src={
+            "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" +
+            props.item.card.info.imageId
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantItemCard;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantItemCard from "./RestaurantItemCard";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
-  //const { id } = useParams();
-  const [resList, setResList] = useState();
+  const { id } = useParams();
+  console.log(id);
+  const [resList, setResList] = useState(null);
   useEffect(() => {
     getRestaurantData();
   }, []);
 
   async function getRestaurantData() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3164945&lng=78.03219179999999&restaurantId=657142&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3164945&lng=78.03219179999999&restaurantId={657142}&submitAction=ENTER"
     );
     const json = await data.json();
     const itemCards =
-      json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card
-        .itemCards;
-    console.log(itemCards);
+      json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+        ?.card?.itemCards;
     setResList(itemCards);
-    console.log(resList);
   }
-  return (
+
+  return !resList ? (
+    <Shimmer />
+  ) : (
     <div>
-      {/* {resList.map((item) => {
-        return <RestaurantItemCard item={item} />;
-      })} */}
+      <p className="font-5xl font-bold w-9/12 mx-auto">Recommended</p>
+      {resList.map((item) => {
+        return <RestaurantItemCard item={item} key={item.card.info.id} />;
+      })}
     </div>
   );
 };
